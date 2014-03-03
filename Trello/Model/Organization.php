0ea5e825839b8e2b4d788acb5259532bf4e4d5de
Ashwin Surajbali <ashwin@teedot.co>
2014-03-03 16:24:24 -0500

    getBoards method for Organizations

--- Trello/Model/Organization.php
+++ Trello/Model/Organization.php
@@ -6,4 +6,17 @@ class Organization extends Object {
 
     protected $_model = 'organizations';
 
+    public function getBoards(array $params = array()){
+
+        $data = $this->getPath('boards', $params);
+
+        $tmp = array();
+        foreach ($data as $item){
+            array_push($tmp, new \Trello\Model\Board($this->getClient(), $item));
+        }
+
+        return $tmp;
+
+    }
+
 }
